7e390fe4e8847275774485d4336b9d2a39cac6dc
Lobay <32384708+Lobay@users.noreply.github.com>
2017-10-06 23:16:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ cin>>m;
 cin>>n;
 int a[m][n];
 int b[m][n];
+int c[m][n];
 for (int i=0; i<m; i++)
         for(int j=0; j<n; j++)
 {
@@ -15,11 +16,12 @@ for (int i=0; i<m;  i++)
         for(int j=0; j<n; j++)
 {
         cin >> b[i][j];
+        c[m][n]=a[m][n]+b[m][n];
 }
 for (int i=0; i<m; i++)
         for(int j=0; j<n; j++)
 {
-        cout << a[i][j]+b[i][j]<<" ";
+        cout << c[i][j]<<" ";
 }
 return 0;
 } 
